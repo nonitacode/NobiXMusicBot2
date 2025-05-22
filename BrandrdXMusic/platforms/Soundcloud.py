d30b219e1d1b78db49398e5aa694718b111c4007
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2025-05-22 16:50:31 +0530

    Update Soundcloud.py

--- BrandrdXMusic/platforms/Soundcloud.py
+++ BrandrdXMusic/platforms/Soundcloud.py
@@ -4,7 +4,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ChampuMusic.utils.formatters import seconds_to_min
+from BrandrdXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
