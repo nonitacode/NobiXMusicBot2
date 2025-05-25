0b2df1e267a1c13e9790d119e316f89538539c45
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2025-05-25 17:24:48 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -105,7 +105,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/tabahi_tabahi",
+                url="https://t.me/ll_sanam_ll",
             ),
         ],
     ]
