b73afa47292bb1fe4e50b5a709f07b76363053f1
Sunny05-bit <sunnykumar9721665358@gmail.com>
2025-06-26 21:29:13 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -35,15 +35,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Githubsoniaryan2/SanamMusicBot",
+    "https://github.com/Sunny05-bit/NewMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_sanam_ll")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+AzTsVLEA00YyMGE9")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/musicsupdate")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+pcz6Xuoq-_k0Y2Q1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -89,13 +89,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/mhbocc.jpg"
+    "START_IMG_URL", "https://i.ibb.co/SDB68PDg/Img2url-bot.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/mhbocc.jpg"
+    "PING_IMG_URL", "https://i.ibb.co/SDB68PDg/Img2url-bot.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+STATS_IMG_URL = "https://i.ibb.co/SDB68PDg/Img2url-bot.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/ebc4dc6357be06e08a3ed.jpg"
